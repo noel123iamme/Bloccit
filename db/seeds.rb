41ad3ee98b94e0a1beecc4ff9ec2171297e1b5c7
Noel Deguzman <noel.deguzman@me.com>
2015-03-25 23:59:35 -0700

    Complete Seed Data Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@ require 'faker'
     body:  Faker::Lorem.paragraph
   )
 end
+
+# Create unique post
+if Post.where({title: "My unique post"}).count == 0
+  Post.create!(
+    title: "My unique post",
+    body:  "Body text of my unique post"
+  )
+end
+
 posts = Post.all
 
 # Create Comments
@@ -17,6 +26,15 @@ posts = Post.all
   )
 end
 
+# Create unique comment
+if Comment.where({body: "My unique comment"}).count == 0
+  Comment.create!(
+    post: posts.sample,
+    body: "My unique comment"
+  ) 
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments create"
+
