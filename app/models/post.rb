fa76939cc7188eb2e358cf545869e3ee9592f352
Noel Deguzman <noel.deguzman@me.com>
2015-04-02 21:45:14 -0700

    Complete Associations Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  scope :ordered_by_title, -> { order(:title) }
+  scope :ordered_by_reverse_created_at, -> { order(created_at: :desc) }
 end
