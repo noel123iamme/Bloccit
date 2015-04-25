407a206264aaede4d42f489322480c4ff4ed7912
Noel Deguzman <noel.deguzman@me.com>
2015-04-24 20:50:08 -0700

    intro-to-rspec-assignment complete

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+ 
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect( @vote.value ).to eq( 1 || -1 )
+      end
+    end
+  end
+end
