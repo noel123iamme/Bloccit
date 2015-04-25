79284767088be326016b641c894127ad9ea18631
Noel Deguzman <noel.deguzman@me.com>
2015-04-25 00:50:23 -0700

    voting checkpoint complete

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
- 
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @vote.value ).to eq( 1 || -1 )
+        post = Post.create(title: 'post title', body: 'post body must be pretty long.')
+
+        vote = Vote.create(value: 1, post: post)
+        expect( vote.value ).to eq(1)
+
+        vote = Vote.create(value: -1, post: post)
+        expect( vote.value ).to eq(-1)
       end
     end
   end
