5202a044074d0c52352754554741121efbd6618a
Noel Deguzman <noel.deguzman@me.com>
2015-04-26 23:58:09 -0700

    mocking with rspec checkpoint complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,10 +9,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
-
-  after_create :create_vote
+  validates :topic, presence: true
+  validates :user, presence: true
 
   def up_votes
      votes.where(value: 1).count
@@ -26,15 +24,13 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
 
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
-  end
-
-  private
-
-  def create_vote
-    user.votes.create(value: 1, post: self)
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 describe Vote do
+  include TestFactories
+
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        post = Post.create(title: 'post title', body: 'post body must be pretty long.')
+        up_vote = Vote.new(value: 1)
+        expect( up_vote.valid? ).to eq(true)
 
-        vote = Vote.create(value: 1, post: post)
-        expect( vote.value ).to eq(1)
+        down_vote = Vote.new(value: -1)
+        expect( down_vote.valid? ).to eq(true)
 
-        vote = Vote.create(value: -1, post: post)
-        expect( vote.value ).to eq(-1)
+        invalid_vote = Vote.new(value: 2)
+        expect( invalid_vote.valid? ).to eq(false)
       end
+    end
+  end
+
+  describe 'after_save' do
+    it "calls `Post#update_rake` after save" do
+      @user = authenticated_user
+      @post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 end
