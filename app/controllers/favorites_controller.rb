a4937b48a4c2b25482022ddc18690e4229b350ec
Noel Deguzman <noel.deguzman@me.com>
2015-04-27 21:31:01 -0700

    favoriting checkpoint completed

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,25 @@
+class FavoritesController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: post)
+    if favorite.save
+      flash[:notice] = "Favorite was saved."
+      redirect_to [post.topic, post]
+    else
+      flash[:error] = "There was an error saving.  Please try again."
+      redirect_to [post.topic, post]
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    favorite = Favorite.find(params[:id])
+    if favorite.destroy
+      flash[:notice] = "Favorite was removed."
+      redirect_to [post.topic, post]
+    else
+      flash[:error] = "There was an error removing.  Please try again."
+      redirect_to [post.topic, post]
+    end
+  end
+end
