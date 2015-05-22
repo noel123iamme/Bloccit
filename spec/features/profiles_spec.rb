9bd38987a0e64ba192efb2a6ab05da6801e34fe3
Noel Deguzman <noel.deguzman@me.com>
2015-05-21 23:01:33 -0700

    public-profiles-assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "Visiting profiles" do
  
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
  
   before do
     @user = authenticated_user
@@ -13,6 +15,21 @@ describe "Visiting profiles" do
   end
  
   describe "not signed in" do
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
+  end
+ 
+  before do
+    login_as(@user, :scope => :user)
+  end
+
+  describe "signed in" do
     it "shows profile" do
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
