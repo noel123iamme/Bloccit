afb2851af6f5afd39d1c7361e95982bb56ae7210
Noel Deguzman <noel.deguzman@me.com>
2015-06-01 22:54:30 -0700

    creating-with-ajax-checkpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.visible_to(current_user)
+    @posts = @user.posts.visible_to(current_user).paginate(page: params[:page], per_page: 10)
     @comments = @user.comments
   end
 
